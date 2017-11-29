febc795d19f418bbee4716aac9ae3bc069601143
pribault <paul@tempow.com>
2017-11-29 20:35:41 +0100

    removing save files

--- src/save_colleen.c
+++
@@ -1,3 +0,0 @@
-#include <stdio.h>
-/*rainboooooow 🌈*/
-int main(void){char q='"';char bs='\\';char *nl="\n";char *s="#include <stdio.h>%s/*rainboooooow 🌈*/%sint main(void){char q='%c';char bs='%c%c';char *nl=%c%cn%c;char *s=%c%s%c;/*in my world there only poneys, they eat rainbows and they poop butterfly ! :D*/printf(s,nl,nl,q,bs,bs,q,bs,q,q,s,q);return (0);}";/*in my world there only poneys, they eat rainbows and they poop butterfly ! :D*/printf(s,nl,nl,q,bs,bs,q,bs,q,q,s,q);return (0);}
